48b90216b4f835db206fb1c403589eeeea6a9c28
Hans Maes <Hans.Maes@SolidSyntax.net>
2017-03-14 20:14:58 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './src/main.js'
+    ],
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+	{	test: /\.jsx?$/,
+        	loader: 'babel-loader',
+		exclude: /node_modules/,
+        	options: {
+            		plugins: ['transform-runtime'],
+            		presets: ['es2015']
+        	}       
+	},
+	{
+		test: /\.hbs$/,loader:'handlebars-loader'
+	} ]
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+		title: 'Intro to webpack',
+		template: 'src/index.html'
+	})
+    ]
+};
